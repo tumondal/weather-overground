84d0820ea259b8ca34aca2003b73bf0591d06c4b
Tuhin Mondal <tuhin.4mondal@gmail.com>
2025-06-16 10:25:43 -0700

    Added imagery

--- WeatherFrame.java
+++ WeatherFrame.java
@@ -9,27 +9,26 @@ import java.util.ArrayList;
 
 public class WeatherFrame extends JFrame {
 
+    private JLabel img;
     private JLabel tempLabel;
     private JLabel weatherLabel;
     private JLabel windLabel;
     private JLabel timeLabel;
     private JLabel errorLabel;
-
+    ArrayList<JLabel> labels = new ArrayList<JLabel>();
 
     public WeatherFrame() {
 
         this.setResizable(false);
 
         setTitle("Weather Overground");
-        setSize(400, 350);
+        setSize(400, 550);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15)); //padding
-        System.out.println(panel.getBackground());
-
 
         JTextField cityInput = new JTextField(15);
         JButton searchButton = new JButton("Search");
@@ -40,6 +39,13 @@ public class WeatherFrame extends JFrame {
         errorLabel = new JLabel("");
         errorLabel.setForeground(Color.RED);
 
+        ImageIcon clearImage = new ImageIcon("images/clear.png");
+        ImageIcon cloudyImage = new ImageIcon("images/cloudy.png");
+        ImageIcon rainImage = new ImageIcon("images/rain.png");
+        ImageIcon snowImage = new ImageIcon("images/snow.png");
+
+        img = new JLabel(clearImage);
+
         JLabel titleLabel = new JLabel("Weather Overground");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 25));
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -47,6 +53,8 @@ public class WeatherFrame extends JFrame {
         cityInput.setAlignmentX(Component.CENTER_ALIGNMENT);
         cityInput.setHorizontalAlignment(JTextField.CENTER);
         cityInput.setMaximumSize(new Dimension(350, 45));
+
+        img.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         tempLabel.setFont(new Font("Arial", Font.BOLD, 40));
 
@@ -59,7 +67,7 @@ public class WeatherFrame extends JFrame {
         timeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         errorLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        ArrayList<JLabel> labels = new ArrayList<JLabel>();
+
 
         panel.add(titleLabel);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
@@ -69,6 +77,9 @@ public class WeatherFrame extends JFrame {
         panel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         panel.add(searchButton);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        panel.add(img);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
         panel.add(tempLabel);
@@ -94,6 +105,7 @@ public class WeatherFrame extends JFrame {
             String city = cityInput.getText().trim();
             if (city.isEmpty()) {
                 errorLabel.setText("Please enter a city name!");
+                img.setIcon(null);
                 tempLabel.setText("");
                 weatherLabel.setText("");
                 windLabel.setText("");
@@ -108,6 +120,7 @@ public class WeatherFrame extends JFrame {
 
             if (coordinates == null) {
                 errorLabel.setText("City not found!");
+                img.setIcon(null);
                 tempLabel.setText("");
                 weatherLabel.setText("");
                 windLabel.setText("");
@@ -126,6 +139,13 @@ public class WeatherFrame extends JFrame {
                 //Update GUI labels instead of just printing
                 tempLabel.setText(data.current_weather.temperature + "°C");
                 weatherLabel.setText(data.current_weather.convertWeatherCode());
+                switch (data.current_weather.convertWeatherCode()) {
+                    case "Clear" -> img.setIcon(clearImage);
+                    case "Cloudy" -> img.setIcon(cloudyImage);
+                    case "Foggy" -> img.setIcon(cloudyImage);
+                    case "Rain" -> img.setIcon(rainImage);
+                    case "Snow" -> img.setIcon(snowImage);
+                }
                 windLabel.setText("Wind Speed: " + data.current_weather.windspeed + " km/h");
                 timeLabel.setText("Time: " + data.getTime());
                 if (data.current_weather.is_day == 0) {
@@ -139,8 +159,10 @@ public class WeatherFrame extends JFrame {
                         labels.get(i).setForeground(Color.BLACK);
                     }
                 }
+
             } else {
                 errorLabel.setText("Could not load weather data!");
+                img.setIcon(null);
                 tempLabel.setText("");
                 weatherLabel.setText("");
                 windLabel.setText("");
